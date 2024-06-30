d04c36a0799749a1bd3da01efc2518f08b5e4a55
Rudi Antoni <rudiantoni2013@gmail.com>
2024-06-30 05:25:42 -0300

    started and finished form control

---
+++ course_02/15_formulario_reactive/01_arquivos/proj_01/my-first-project/src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-forms.component.html',
+  styleUrl: './reactive-forms.component.scss',
+})
+export class ReactiveFormsComponent {
+  name = new FormControl('Rudi Antoni');
+
+  updateName() {
+    this.name.setValue('Nome de teste');
+  }
+}
---
+++ course_02/15_formulario_reactive/01_form_control.md
@@ -0,0 +1,8 @@
+# Curso de Angular 2 (v17+) Typescript do Básico ao Avançado
+
+## 15.1. Form Control
+[[Home - Curso 2]](../../README.md#curso-2)<br />
+
+- Diretório de arquivos: [01_arquivos](./01_arquivos/)
+- Projetos
+  - [Projeto 1](./01_arquivos/proj_01/) [(iniciado em)](#form-control)
